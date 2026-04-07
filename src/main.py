797da8032bba85234c8f39f993d106bdc73f3c05
Kawelobo <kawe.lobo@sempreceub.com>
2026-04-07 18:52:03 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
-print("VidaOrganizada funcionando!")
+tarefas = []
+
+def adicionar():
+    tarefa = input("Digite uma tarefa: ")
+    if tarefa.strip() == "":
+        print("Tarefa inválida!")
+        return
+    tarefas.append(tarefa)
+    print("Tarefa adicionada!")
+
+def listar():
+    if not tarefas:
+        print("Nenhuma tarefa.")
+        return
+    for i, t in enumerate(tarefas):
+        print(f"{i} - {t}")
+
+def menu():
+    while True:
+        print("\n1-Adicionar  2-Listar  0-Sair")
+        op = input("Escolha: ")
+
+        if op == "1":
+            adicionar()
+        elif op == "2":
+            listar()
+        elif op == "0":
+            break
+        else:
+            print("Opção inválida")
+
+menu()
